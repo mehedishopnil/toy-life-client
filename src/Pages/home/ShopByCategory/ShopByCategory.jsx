f974f01c3a88147680e790c985418fc9d921ef39
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-10-13 23:59:20 +0600

    dgs

--- src/Pages/home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/home/ShopByCategory/ShopByCategory.jsx
@@ -1,92 +1,79 @@
-import  { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import { AuthContext } from '../../../providers/AuthProvider';
 
 const ShopByCategory = () => {
-  const {productsData} = useContext(AuthContext);
-  console.log(productsData);
-
-    const categories = ['math', 'science', 'engineering']; // Define your categories here
-    const [activeCategory, setActiveCategory] = useState(categories[0]); // Set your initial active category here
+  const { productsData } = useContext(AuthContext);
+  const categories = ['Math', 'Science', 'Engineering'];
+  const [activeCategory, setActiveCategory] = useState(categories[0]);
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(12);
 
-    
-      // Handle tab click
-      const handleTabClick = (category) => {
-        setActiveCategory(category);
-        setCurrentPage(1);
-      };
-    
-      // Pagination
-      const indexOfLastProduct = currentPage * productsPerPage;
-      const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-      const currentProducts = productsData.slice(indexOfFirstProduct, indexOfLastProduct);
-    
-      // Render products
-      const renderProducts = currentProducts.map((product) => (
-        <div key={product.id} className="p-4">
-          <div className="bg-white rounded-lg shadow-md p-4">
-            <img src={product.image} alt={product.name} className="w-32 h-32 mx-auto mb-2" />
-            <h2 className="text-xl font-semibold">{product.name}</h2>
-            <p className="text-white">${product.price}</p>
-            <p className="text-yellow-400">Rating: {product.rating}</p>
-            <button className="bg-primary text-white rounded-md px-4 py-2 mt-2">View Details</button>
-          </div>
-        </div>
-      ));
+  const handleTabClick = (category) => {
+    setActiveCategory(category);
+    setCurrentPage(1);
+  };
 
-    return (
-        <div>
-            <div className='py-8'>
-            <h2 className=' text-center text-5xl font-bold'>Shop By Category</h2>
-            </div>
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const filteredProducts = productsData.filter((product) => product.category === activeCategory);
+  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
 
-            <div>
-            <div>
-      <Tabs>
-        <TabList>
+  return (
+    <div>
+      <div className="py-8">
+        <h2 className="text-center text-5xl font-bold">Shop By Category</h2>
+      </div>
+
+      <div className="bg-primary py-4 px-4 space-x-8">
+        <Tabs>
+          <TabList className="flex justify-center py-5 gap-8">
+            {categories.map((category) => (
+              <Tab
+                key={category}
+                className={`${
+                  activeCategory === category ? 'bg-secondary text-white' : 'bg-gray-100 text-gray-600'
+                } px-4 py-2 cursor-pointer rounded-full`}
+                onClick={() => handleTabClick(category)}
+              >
+                {category}
+              </Tab>
+            ))}
+          </TabList>
+
           {categories.map((category) => (
-            <Tab
-              key={category}
-              className={`${
-                activeCategory === category ? 'bg-secondary text-white' : 'bg-primary text-gray-600'
-              } px-4 py-2 cursor-pointer`}
-              onClick={() => handleTabClick(category)}
-            >
-              {category}
-            </Tab>
+            <TabPanel key={category}>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {currentProducts.map((product) => (
+                  <div key={product.id} className="bg-white rounded-lg shadow-md p-4">
+                    <img src={product.image} alt={product.name} className="w-full h-64 object-cover" />
+                    <h2 className="text-xl font-semibold mt-4">{product.product_name}</h2>
+                    <p className="text-white text-lg mt-2">${product.price}</p>
+                    <p className="text-yellow-400">Rating: {product.rating}</p>
+                    <button className="bg-secondary text-white rounded-md px-4 py-2 mt-2">View Details</button>
+                  </div>
+                ))}
+              </div>
+
+              <div className="mt-4 flex justify-center">
+                {Array.from({ length: Math.ceil(filteredProducts.length / productsPerPage) }).map((_, index) => (
+                  <button
+                    key={index}
+                    className={`mx-2 px-4 py-2 rounded-md ${
+                      currentPage === index + 1 ? 'bg-secondary text-white' : 'bg-primary text-gray-600'
+                    }`}
+                    onClick={() => setCurrentPage(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+              </div>
+            </TabPanel>
           ))}
-        </TabList>
-
-        {categories.map((category) => (
-          <TabPanel key={category}>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {renderProducts}
-            </div>
-
-            {/* Pagination */}
-            <div className="mt-4 flex justify-center">
-              {Array.from({ length: Math.ceil(productsData.length / productsPerPage) }).map((_, index) => (
-                <button
-                  key={index}
-                  className={`mx-2 px-4 py-2 rounded-md ${
-                    currentPage === index + 1 ? 'bg-secondary text-white' : 'bg-primary text-gray-600'
-                  }`}
-                  onClick={() => setCurrentPage(index + 1)}
-                >
-                  {index + 1}
-                </button>
-              ))}
-            </div>
-          </TabPanel>
-        ))}
-      </Tabs>
+        </Tabs>
+      </div>
     </div>
-            </div>
-
-        </div>
-    );
+  );
 };
 
 export default ShopByCategory;
